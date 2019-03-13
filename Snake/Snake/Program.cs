385483f69ca218de481018992cf2a25c7e639362
vtufvty <udaff.80@mail.ru>
2019-03-13 22:00:52 +0400

    списки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,30 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '@';
+            Point p1 = new Point( 2, 3, '@' );
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point( 3, 4, '@');       
             p2.Draw();
+
+            Point p3 = new Point(4, 5, '@');
+            p3.Draw();
+
+            Point p4 = new Point(5, 6, '@');
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+       
 
             Console.ReadLine();
 
